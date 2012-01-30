6f2892275347cf6fbdf0e7ba4bd84e8a61e891f5
Daniel Ge <dange@seas.upenn.edu>
2012-01-29 23:41:09 -0500

    Modified html_out.py to generate HTML that is more easily readable. Also made an actual README

--- html_out.py
+++ html_out.py
@@ -9,7 +9,7 @@ def generate_html(header, data_entry, title_index):
             <h1>""" + name + """'s Application</h1>
     """
     for i in range(len(header)):
-        html += "<h2>" + header[i] + "</h2>"
+        html += "<h2>" + header[i] + "</h2>\n"
         html += htmlify(data_entry[i])
 
     html += """
@@ -20,6 +20,6 @@ def generate_html(header, data_entry, title_index):
 
 def htmlify(data_str):
     output = "<p>"
-    output += data_str.replace("\r\n", "</p><p>")
-    output += "</p>"
+    output += data_str.replace("\r\n", "</p>\n<p>")
+    output += "</p>\n"
     return output
