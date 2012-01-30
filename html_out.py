41cd26201a9da8f665d89a4c8ee6f3d30880daf4
Daniel Ge <dange@seas.upenn.edu>
2012-01-29 23:34:27 -0500

    Wrote html_out.py and updated main.py to output valid html. (It works!)

---
+++ html_out.py
@@ -0,0 +1,25 @@
+def generate_html(header, data_entry, title_index):
+    name = data_entry[title_index]
+    html = """<!DOCTYPE html>
+    <html>
+        <head>
+            <title>""" + name + """</title>
+        </head>
+        <body>
+            <h1>""" + name + """'s Application</h1>
+    """
+    for i in range(len(header)):
+        html += "<h2>" + header[i] + "</h2>"
+        html += htmlify(data_entry[i])
+
+    html += """
+        </body>
+    </html>"""
+
+    return html
+
+def htmlify(data_str):
+    output = "<p>"
+    output += data_str.replace("\r\n", "</p><p>")
+    output += "</p>"
+    return output
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from csv_reader import Parser
+from html_out   import generate_html
 
 csv_file = "app-utf8.csv"
 parser = Parser(csv_file)
-parser.print_contents()
+
+TITLE = 3
+
+for entry in parser.data:
+    out_file = open(entry[TITLE] + ".html", 'w')
+    out_file.write(generate_html(parser.header, entry, TITLE))
+    out_file.close()
+
+
