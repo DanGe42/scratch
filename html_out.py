45abe75bc691b0b660cd9bf6ad05720ef7d01651
Daniel Ge <dange@seas.upenn.edu>
2012-01-29 23:57:44 -0500

    Added CSS to prettify the application

--- html_out.py
+++ html_out.py
@@ -4,12 +4,13 @@ def generate_html(header, data_entry, title_index):
     <html>
         <head>
             <title>""" + name + """</title>
+            <link href="css/styles.css" rel="stylesheet" type="text/css" />
         </head>
         <body>
             <h1>""" + name + """'s Application</h1>
     """
     for i in range(len(header)):
-        html += "<h2>" + header[i] + "</h2>\n"
+        html += "<h2 class=\"question\">" + header[i] + "</h2>\n"
         html += htmlify(data_entry[i])
 
     html += """
@@ -19,7 +20,7 @@ def generate_html(header, data_entry, title_index):
     return html
 
 def htmlify(data_str):
-    output = "<p>"
-    output += data_str.replace("\r\n", "</p>\n<p>")
+    output = "<p class=\"response\">"
+    output += data_str.replace("\r\n", "</p>\n<p class=\"response\">")
     output += "</p>\n"
     return output
