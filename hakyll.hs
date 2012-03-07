e250b54169a2d62de343e250e80961e34ee9ddeb
Daniel Ge <dange@seas.upenn.edu>
2012-03-07 03:18:23 -0500

    Added more CSS and currently unlinked projects page

--- hakyll.hs
+++ hakyll.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Arrow ((>>>))
+import Control.Arrow ((>>>), arr)
+import Data.Monoid (mempty)
 
 import Hakyll
 
@@ -15,8 +16,31 @@ main = hakyll $ do
 
     match "templates/*" $ compile templateCompiler
 
+    {-match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> arr (setField "title" "Home")
+        >>> arr (setField "css" $ insertCSS Nothing)
+        >>> arr (setField "scripts" $ insertJS Nothing)
+        >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler-}
     match (list ["index.html"]) $ do
         route   $ setExtension "html"
         compile $ pageCompiler
             >>> applyTemplateCompiler "templates/default.html"
-            -- >>> relativizeUrlsCompiler
+            >>> relativizeUrlsCompiler
+
+    match "projects.html" $ route idRoute
+    create "/projects/index.html" $ constA mempty
+        >>> arr (setField "title" "Projects")
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> applyTemplateCompiler "templates/projects.html"
+        >>> relativizeUrlsCompiler
+
+insertCSS :: Maybe String -> String
+insertCSS Nothing = []
+insertCSS (Just path) = "<link rel=\"stylesheet\" type=\"text/css\" href=\"" ++ path ++ "\">"
+
+insertJS :: Maybe String -> String
+insertJS Nothing = []
+insertJS (Just path) = "<script type=\"text/javascript\" src=\"" ++ path ++ "\"></script>"
