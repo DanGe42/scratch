29a3a7baee2c32dffe8ce417c4af95ec705713dc
Daniel Ge <daniel@seas.upenn.edu>
2012-07-30 00:18:37 -0700

    Send uploads to upload/<date>/ directory
    It's better than cluttering up the root directory of the app.

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,47 @@
 var fs = require('fs')
   , crypto = require('crypto');
 
+
+// Helper methods for .upload
+
+// This method, despite its name, is limited to one use:
+// it continues if a folder exists, and throws an error if something else
+// weird occurred.
+var throwErrorOr = function (error) {
+    // Error 47 is EEXIST (folder already exists)
+    if (error && error.errno !== 47) {
+        console.log("Failed to create directory '" + error.path + "'");
+        throw error;
+    }
+};
+
+var uploadFile = function(filename, date, buffer, success) {
+    fs.mkdir("uploads", '0777', function(err1) {
+        throwErrorOr(err1);
+
+        var date_folder = "uploads/" +
+                        date.getUTCFullYear() + "-" + date.getUTCMonth() + "-" +
+                        date.getUTCDate();
+
+        fs.mkdir(date_folder, '0777', function(err2) {
+            throwErrorOr(err2);
+
+            var path = date_folder + "/" + filename;
+            fs.writeFile(path, buffer, function(error) {
+                if (error) {
+                    throw error;
+                }
+
+                console.log("Successfully wrote to '" + path + "'");
+
+                if (success) {
+                    success();
+                }
+            });
+        });
+    });
+};
+
 /*
  * POST canvas data to server
  */
@@ -22,23 +63,19 @@ exports.upload = function(req, res) {
     var shasum = crypto.createHash('sha1');
     shasum.update(result[1]);
 
-    var filename = '/tmp/' + shasum.digest('hex').slice(0, 7) + ".png";
+    var now = new Date();
 
-    console.log("Writing image to " + filename + "...");
-
-    fs.writeFile(filename, image, function (err) {
-        if (err) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.write("Something went wrong :(");
-            res.write(err);
-            console.log(err);
+    try {
+        var filename = shasum.digest('hex').slice(0, 7) + ".png";
+        uploadFile(filename, now, image, function() {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.write("Your upload was successful!");
             res.end();
-            return;
-        }
-
-        console.log("Successfully wrote file to " + filename);
-        res.writeHead(200, { "Content-Type": "text/plain" });
-        res.write("Your upload was successful!");
+        });
+    } catch (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.write("Something went wrong :(");
+        console.log(err);
         res.end();
-    });
+    }
 }
