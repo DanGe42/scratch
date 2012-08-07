74d34718b1513a0202a6c4cb47252b3a2062e721
Daniel Ge <dange@seas.upenn.edu>
2012-08-07 01:01:34 -0700

    Schedule cron job and make corrections to package.json
    node-cron does not exist as 1.0 yet, so change to 0.3
    node-cron can depend on the time module for time zone support

--- app.js
+++ app.js
@@ -6,7 +6,8 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mooJob = require('./cron').mooJob();
 
 var app = express();
 
@@ -31,3 +32,5 @@ app.get('/', routes.index);
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
+
+mooJob.start();
--- cron.js
+++ cron.js
@@ -1,4 +1,71 @@
-var cronJob = require('cron').cronJob,
-    config  = require('./config').config;
-    hipchat = require('./lib/hipchat');
+var CronJob = require('cron').CronJob,
+    config  = require('./config').config,
+    hipchat = require('./lib/hipchat'),
+    time = require('time'),
+    cow = require('./lib/cow');
 
+var onComplete = function() {
+  console.log("Job finished at " + new Date());
+};
+var TZ = "America/Los Angeles";
+
+exports.mooJob = function() {
+  // Run this cron job every two hours from 10:00 AM to 8:00 PM every day
+  var job = new CronJob({
+    cronTime: '0 0 9-19/2 * * *',
+    onTick: function() {
+      console.log("Starting Hipchat job...");
+
+      var token = config["hipchat_token"];
+
+      cow.moo(function (error, stdout, stderr) {
+        if (error === null) {
+          var room = "Test";
+
+          hipchat.sendToHipchat(token, room, stdout, function(res) {
+            var data = "";
+            res.on('data', function (chunk) {
+              data += chunk;
+            });
+            res.on('close', function (err) {
+              console.log("The connection terminated unexpectedly.");
+              console.log(err);
+            });
+
+            res.on('end', function () {
+              var parsed = JSON.parse(data);
+              try {
+                if (res.statusCode === 200) {
+                  console.log("200 OK! Message successfully posted to room " + room + ".");
+                  console.log("The status of this request is -- '" + parsed["status"]);
+                }
+                else {
+                  console.log("Bad status code returned: " + res.statusCode);
+                  console.log("Error type: " + parsed["type"]);
+                  console.log("Why: " + parsed["message"]);
+                }
+              } catch (err) {
+                console.log("An internal server error while parsing the response.");
+                console.log(err);
+              }
+            });
+
+          });
+        }
+        else {
+          console.log("An error occurred while executing the shell command.");
+          console.log("Maybe stderr might help?");
+          console.log(stderr);
+          console.log("Or maybe the error dump from Node?");
+          console.log(error);
+        }
+      });
+    },
+
+    onComplete: onComplete,
+    start: false,
+    timeZone: TZ
+  });
+
+  return job;
+};
